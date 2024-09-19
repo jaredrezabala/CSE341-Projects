977627a89611f68a406d5a9248d996acad41a022
jaredrezabala <rezabalajared@gmail.com>
2024-09-18 22:08:00 -0500

    project1 finished

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,23 @@
 const mongodb = require('../modules/mongodb')
 const mongoConnect = require('mongodb')
 const ObjectId = mongoConnect.ObjectId
+
+const getAll = async(req, res) =>{
+    const result = await mongodb.getDatabase().db().collection('users').find()
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.status(200).json(users)
+    })
+}
+const getSingle = async(req, res) => {
+    const userId = new ObjectId(req.params.id)
+    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId})
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.status(200).json(users[0])
+    })
+}
+module.exports = {
+    getAll,
+    getSingle
+}
